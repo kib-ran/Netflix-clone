df99739e9403a50fca2a9bfa415e9066e0c726dc
kibran meng <kibran29mengistu@gmail.com>
2025-09-05 17:20:38 +0300

    banner

---
+++ my-app/src/components/Banner/Banner.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import axios from "../../utils/axios";
+import requests from "../../utils/requests";
+import "./Banner.css";
+
+function Banner() {
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+        console.log(request);
+        const selectedMovie =
+          request.data.results[
+            Math.floor(Math.random() * request.data.results.length)
+          ];
+        setMovie(selectedMovie);
+        console.log("Selected movie:", selectedMovie); 
+      } catch (error) {
+        console.error(" Error fetching movie data:", error); 
+      }
+    }
+    fetchData();
+  }, []);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button play" aria-label="Play movie">
+            Play
+          </button>
+          <button className="banner_button myList" aria-label="Add to My List">
+            My List
+          </button>
+        </div>
+        <h1 className="banner_description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner_fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
